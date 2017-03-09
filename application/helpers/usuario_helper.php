f0a05a67befee3311a8bcc1a53a6f7a87281dd35
Alisson Acioli <alissonacioli@hotmail.com>
2017-03-09 02:52:44 -0300

    helper de info do usuário

---
+++ application/helpers/usuario_helper.php
@@ -0,0 +1,26 @@
+<?php
+function usuario($column){
+
+    $_this =& get_instance();
+
+    if($_this->session->has_userdata('userid')){
+
+        $id = $_this->session->userdata('userid');
+
+        $_this->db->where('id', $id);
+        $query = $_this->db->get('usuarios');
+
+        if($query->num_rows() > 0){
+
+            $row = $query->row();
+
+            return $row->$column;
+        }else{
+            return 'Não encontrado';
+        }
+    }else{
+
+        return 'Não encontrado';
+    }
+}
+?>
