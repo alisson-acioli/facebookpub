0729987f0fb51bce2d476d2f9386d752ca5cf029
Alisson Acioli <alissonacioli@hotmail.com>
2017-03-09 02:50:44 -0300

    Carregando library do facebook e um novo helper de usuário

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('database', 'email', 'session');
+$autoload['libraries'] = array('database', 'email', 'session', 'facebook');
 
 /*
 | -------------------------------------------------------------------
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url', 'configuracoes');
+$autoload['helper'] = array('url', 'configuracoes', 'usuario');
 
 /*
 | -------------------------------------------------------------------
