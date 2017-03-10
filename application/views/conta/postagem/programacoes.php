db67e836a41410794dfc6771ebc613735572681f
Alisson Acioli <alissonacioli@hotmail.com>
2017-03-10 00:00:59 -0300

    View de programações feitas

---
+++ application/views/conta/postagem/programacoes.php
@@ -0,0 +1,47 @@
+<section class="site-content">
+   
+   <h3>Suas programações</h3>
+
+   <p>Quando você faz uma publicação você tem a possibilidade de programar as postagens, assim você não precisa se preocupar em entrar todo dia no facebook para postar em sua página.</p>
+   <p>Caso você queria <b>Cancelar</b> uma programação, basta clicar em <u>Excluir</u>, assim a programação não será postada.</p>
+
+   <table class="table">
+   <thead class="thead-inverse">
+     <tr>
+       <th>Data</th>
+       <th>Foto</th>
+       <th>Descrição</th>
+       <th>Tipo</th>
+       <th>Status</th>
+       <th>Ação</th>
+     </tr>
+   </thead>
+   <tbody>
+     <tr>
+       <th scope="row">10/03/2017</th>
+       <th>&nbsp;</th>
+       <td>Quer ser o tio do barzinho mais famoso do banco?</td>
+       <td>Texto</td>
+       <td><span class="label label-info">Processando...</span></td>
+       <td><a href="#">Excluir</a></td>
+     </tr>
+     <tr>
+       <th scope="row">09/03/2017</th>
+       <th>&nbsp;</th>
+       <td>Lorem ipsum dollor dseol colmys</td>
+       <td>Link</td>
+       <td><span class="label label-danger">Cancelado</span></td>
+       <td>&nbsp;</td>
+     </tr>
+     <tr>
+       <th scope="row">05/03/2017</th>
+       <th>&nbsp;</th>
+       <td>Lorem ipsum dollor dseol colmys</td>
+       <td>Texto</td>
+       <td><span class="label label-success">Publicado</span></td>
+       <td><a href="#">Visualizar</a></td>
+     </tr>
+   </tbody>
+   </table>
+</section>
+<!-- /.site-content -->
