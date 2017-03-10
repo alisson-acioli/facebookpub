cea956857aec8f19e7d537ef83cd519bbd35fe53
Alisson Acioli <alissonacioli@hotmail.com>
2017-03-10 01:07:55 -0300

    Gráfico de relatórios

--- application/views/conta/templates/footer.php
+++ application/views/conta/templates/footer.php
@@ -32,7 +32,7 @@
 <script src="<?php echo base_url();?>assets/global/js/plugins/firstlitter.js"></script>
 <script src="<?php echo base_url();?>assets/global/js/plugins/video-modal.js"></script>
 
-<!-- plugins for the current page -->
+
 <script src="<?php echo base_url();?>assets/vendor/bower_components/chartist/dist/chartist.min.js"></script>
 <script src="<?php echo base_url();?>assets/vendor/jvectormap/jquery-jvectormap.min.js"></script>
 <script src="<?php echo base_url();?>assets/vendor/jvectormap/maps/jquery-jvectormap-world-mill.js"></script>
@@ -51,6 +51,84 @@
 <script src="<?php echo base_url();?>assets/js/navbar.js"></script>
 <script src="<?php echo base_url();?>assets/js/menubar.js"></script>
 
+
+<script src="https://code.highcharts.com/highcharts.js"></script>
+<script src="https://code.highcharts.com/modules/exporting.js"></script>
+
+<script>
+Highcharts.chart('postagem_dia', {
+
+    title: {
+        text: 'Postagem por dia'
+    },
+
+    subtitle: {
+        text: 'Quantidade de postagens feitas por dia'
+    },
+
+    yAxis: {
+        title: {
+            text: 'Quantidade'
+        }
+    },
+
+    xAxis: {
+      categories: [2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016]
+    },
+
+    credits:{
+      enabled: false
+    },
+
+    legend:{
+      enabled: false
+    },
+
+    series: [{
+        name: 'Postagens',
+        data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175]
+    }]
+
+});
+</script>
+
+<script>
+Highcharts.chart('curtidas_dia', {
+
+    title: {
+        text: 'Curtidas por dia'
+    },
+
+    subtitle: {
+        text: 'Curtidas total de todas as páginas por dia'
+    },
+
+    yAxis: {
+        title: {
+            text: 'Quantidade'
+        }
+    },
+
+    xAxis: {
+      categories: ['01/03/2017', '02/03/2017', '03/03/2017', '04/03/2017', '05/03/2017', '06/03/2017', '07/03/2017', '08/03/2017']
+    },
+
+    credits:{
+      enabled: false
+    },
+
+    legend:{
+      enabled: false
+    },
+
+    series: [{
+        name: 'Curtidas',
+        data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175]
+    }]
+
+});
+</script>
+
 <!-- theme customizer -->
 <script src="<?php echo base_url();?>assets/examples/js/theme-customizer.js"></script>
 
@@ -61,5 +139,7 @@ if(isset($jsLoader)){
    }
 }
 ?>
+
+
 </body>
 </html>
