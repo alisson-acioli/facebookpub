8d99cdf5be32646743e11ac114dfba2d41e1dfdb
Alisson Acioli <alissonacioli@hotmail.com>
2017-03-11 20:54:02 -0300

    Carregando helper de "data"

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url', 'configuracoes', 'usuario');
+$autoload['helper'] = array('url', 'configuracoes', 'usuario', 'data');
 
 /*
 | -------------------------------------------------------------------
