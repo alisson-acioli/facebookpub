2abffb2e50253d922b51e61843db27abb627d96e
Alisson Acioli <alissonacioli@hotmail.com>
2017-03-24 20:06:50 -0300

    Tradução padrão das mensagens do CodeIgniter mudado para inglês

--- application/config/config.php
+++ application/config/config.php
@@ -76,7 +76,7 @@ $config['url_suffix'] = '';
 | than english.
 |
 */
-$config['language']	= 'pt-br';
+$config['language']	= 'english';
 
 /*
 |--------------------------------------------------------------------------
