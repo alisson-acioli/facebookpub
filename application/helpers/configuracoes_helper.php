e8bd9487a1d65011954fdd595506194dd5ee0595
Alisson Acioli <alissonacioli@hotmail.com>
2017-03-31 03:04:50 -0300

    Estrutura da verificação de purchase adicionado

--- application/helpers/configuracoes_helper.php
+++ application/helpers/configuracoes_helper.php
@@ -60,4 +60,39 @@ function TimesZones() {
     }
     return $zones_array;
 }
+
+function verify($purchaseCode) {
+    if(!session("verify")){
+        $purchaseCode = ($purchaseCode == "")?"none":$purchaseCode;
+        $result = false; // have we got a valid purchase code?
+        $our_item_id = 15279075; // check if they've bought this item id.
+        $username = 'tienpham1606'; // authors username
+        $apiKey = 'xpny3lng9htbi7j6dyn84j556ixu4umv'; // api key from my account area
+
+        // Open cURL channel
+        $ch = curl_init();
+
+        // Set cURL options
+        curl_setopt($ch, CURLOPT_URL, "http://marketplace.envato.com/api/edge/$username/$apiKey/verify-purchase:$purchaseCode.json");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_USERAGENT, 'ENVATO-PURCHASE-VERIFY'); //api requires any user agent to be set
+
+        $result = json_decode( curl_exec($ch) , true );
+
+        //check if purchase code is correct
+        if($result != ""){
+            if ( !empty($result['verify-purchase']['item_id']) && $result['verify-purchase']['item_id'] ) {
+                set_session("verify", $result['verify-purchase']);
+                return $result['verify-purchase'];
+            }
+        }elseif($result == ""){ 
+            return true;
+        }
+
+        //invalid purchase code
+        return false;
+    }else{
+        return session("verify");
+    }
+}
 ?>
